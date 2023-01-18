aa77f982be13c67dc000d5807be20caf0ec20dfd
jnguyen657 <58559959+jnguyen657@users.noreply.github.com>
2023-01-17 19:47:08 -0500

    Update highestScorers.py

--- highestScorers.py
+++ highestScorers.py
@@ -1,19 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd     #dataframe
 
 def get_highest_scorer(url):
     r = requests.get(url)
-    sp = BeautifulSoup(r.text, 'lxml')
+    sp = BeautifulSoup(r.text, 'lxml') 
     player = { 
-        'Player name': sp.select_one('a.Anchor_anchor__cSc3P').text.strip().replace('\n', ' '), 
-        'Team': sp.select_one('a.Anchor_anchor__cSc3P').text.strip().replace('\n', ' ')
+        'Player name': sp.select_one('td.who').text.strip().replace('\n', ' '), 
+        'Team': sp.select_one('span.desc').text.strip().replace('\n', ' '),
+        'Total points': sp.select_one('td.value').text.strip().replace('\n', ' '),
     }
-    print(player)
+    print (sp.title.string)
+    return player
 
 
-base_url = 'https://www.nba.com/stats/leaders?SeasonType=Regular+Season&Season='
-nba_season = input("Enter the NBA season to view, in a XXXX-XX format. Example: 2022-23")
-final_url = base_url + nba_season
+base_url = 'https://www.basketball-reference.com/leagues/NBA_' 
+base_url2 = '_leaders.html'
+nba_season = input("Enter the NBA season to view in a XXXX format. Example: 2022-23 would be 2023.")
+final_url = base_url + nba_season + base_url2    
 
-get_highest_scorer(final_url)
-
+results = get_highest_scorer(final_url)
+print(results)
